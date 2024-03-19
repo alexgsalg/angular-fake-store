dd29414b3f49776c36d48b2d7915649a25a230bd
Alex Salgado <alexgsalgado@gmail.com>
2024-03-19 08:38:30 -0300

    feat: add header template and styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 
@@ -9,5 +9,6 @@ import { ButtonComponent } from '../button/button.component';
   styleUrls: ['./header.component.scss'],
   standalone: true,
   imports: [CommonModule, RouterLink, ButtonComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderComponent {}
